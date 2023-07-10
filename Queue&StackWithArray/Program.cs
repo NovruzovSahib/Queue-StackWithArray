4d8bebdc15dc5841c4e6667d8678d64bfa4b689f
Sahib Novruzov <sahib.novruzov33@hotmail.com>
2023-07-10 11:38:21 +0400

    Add project files.

---
+++ Queue&StackWithArray/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+internal class QueueWithArray
+{
+
+    public static void Main(String[] args)
+    {
+        Queue queue = new Queue(5);
+        queue.queueEnqueue(20);
+        queue.queueEnqueue(30);
+        queue.queueEnqueue(40);
+        queue.queueEnqueue(50);
+        queue.ShowQueue();
+
+        Console.WriteLine("After added new queue");
+        queue.queueEnqueue(60);
+        queue.ShowQueue();
+
+        queue.queueDequeue();
+        queue.queueDequeue();
+        queue.queueDequeue();
+        Console.Write("After two queue deletion");
+        queue.ShowQueue();
+
+        queue.queueFront();
+        Console.WriteLine();
+        queue.queueRear();
+    }
+}
+public class Queue
+{
+    private int front, rear, capacity;
+    private int[] queue;
+
+    public Queue(int c)
+    {
+        front = rear = 0;
+        capacity = c;
+        queue = new int[capacity];
+    }
+    public void ShowQueue()
+    {
+        if (front == rear)
+        {
+            Console.WriteLine("Queue is Empty");
+            return;
+        }
+        Console.WriteLine("Queue elements");
+        for (int i = front; i < rear; i++)
+        {
+            Console.WriteLine(queue[i]);
+        }
+    }
+    public void queueEnqueue(int data)
+    {
+        if (capacity == rear)
+        {
+            Console.WriteLine("Queue is full");
+            return;
+        }
+        else
+        {
+            queue[rear] = data;
+            rear++;
+        }
+    }
+    public void queueDequeue()
+    {
+        if (front == rear)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < rear - 1; i++)
+            {
+                queue[i] = queue[i + 1];
+            }
+
+            if (rear < capacity)
+                queue[rear] = 0;
+            rear--;
+        }
+    }
+ 
+    public void queueFront()
+    {
+        if (front == rear)
+        {
+            Console.WriteLine("Queue is Empty");
+            return;
+        }
+        Console.Write("Front Element is: {0}",
+                    queue[front]);
+    }
+    public void queueRear()
+    {
+        if (front == rear)
+        {
+            Console.WriteLine("Queue is Empty");
+            return;
+        }
+        Console.Write("Rear Element is: {0}",
+                    queue[rear]);
+    }
+}
+
+
---
+++ StackWithArray/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+class StackWithArray
+{
+    static void Main(string[] args)
+    {
+        Stack stack = new Stack(5);
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(40);
+        stack.ShowStack();
+
+        Console.WriteLine("After adding new item to stack");
+        stack.Push(50);
+        stack.ShowStack();
+
+        stack.Pop();
+        stack.Pop();
+        Console.Write("After two item removal from stack: ");
+        stack.ShowStack();
+
+        stack.Peek();
+    }
+}
+
+class Stack
+{
+    private int top;
+    private int capacity;
+    private int[] stack;
+
+    public Stack(int size)
+    {
+        top = -1;
+        capacity = size;
+        stack = new int[capacity];
+    }
+
+    public void Push(int item)
+    {
+        if (top == capacity - 1)
+        {
+            Console.WriteLine("Stack is full. Cannot push.");
+            return;
+        }
+
+        stack[++top] = item;
+    }
+
+    public int Pop()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty. Cannot pop.");
+            return -1;
+        }
+
+        return stack[top--];
+    }
+
+    public int Peek()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty. Cannot peek.");
+            return -1;
+        }
+
+        Console.WriteLine("Top element in stack: " + stack[top]);
+        return stack[top];
+    }
+
+    public void ShowStack()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty.");
+            return;
+        }
+
+        Console.WriteLine("Stack elements:");
+        for (int i = top; i >= 0; i--)
+        {
+            Console.WriteLine(stack[i]);
+        }
+    }
+}
